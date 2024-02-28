5101cbe39fb2b93938649f23e4e0100d8b3da92d
andy.tsai <andy5566w@gmail.com>
2024-02-28 17:19:00 +0800

    [vue-project] package vuex module

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,10 @@
 import { createStore } from 'vuex';
+import { post } from '@/store/post/index.js';
+import { comment } from '@/store/comment/index.js';
+import { user } from '@/store/user/index.js';
 
 const store = createStore({
-  modules: {},
+  modules: { post, comment, user },
 });
 
 export default store;

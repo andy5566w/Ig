e8adfe913e91dd0ac11b88d33dac19c991aa7525
andy.tsai <andy5566w@gmail.com>
2024-02-28 17:15:07 +0800

    [vue-project] add vuex

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 import router from './route/routes.js';
+import store from '@/store/index.js';
 
 const app = createApp(App);
 app.config.errorHandler = (err, vm, info) => {
   console.log({ err, vm, info });
 };
 app.use(router);
+app.use(store);
 app.mount('#app');

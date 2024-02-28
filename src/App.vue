0c791e3246b22f1b7560b2ef8538bd8680e61a5d
andy.tsai <andy5566w@gmail.com>
2024-02-28 17:05:12 +0800

    [vue-project] add footer style

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,30 @@
   <template v-else>
     <router-view />
   </template>
+  <footer class="footer" :class="{ inside: $route.name === 'login' }">
+    &copy; All Rights Reserved.
+  </footer>
 </template>
 
 <script setup>
 import Navigator from './components/Navigator.vue';
 import TheLayout from './components/TheLayout.vue';
 </script>
+
+<style scoped lang="scss">
+.header {
+  height: 80px;
+  box-shadow: 0 4px 24px rgba(0, 0, 0, 0.08);
+}
+
+.footer {
+  text-align: center;
+  padding: 38px 0;
+  color: #828282;
+  &.inside {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+  }
+}
+</style>

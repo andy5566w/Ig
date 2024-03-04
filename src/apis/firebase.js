cacddf0f01f27b1c89cb542278e1348373eb586b
andy.tsai <andy5566w@gmail.com>
2024-03-04 16:36:51 +0800

    [vue-project] add new post after uploading.

--- src/apis/firebase.js
+++ src/apis/firebase.js
@@ -1,11 +1,13 @@
-import { db, auth, storage } from '@/apis/auth.js';
+import { auth, db, storage } from '@/apis/auth.js';
 import store from '@/store/index.js';
-import { doc, getDoc, addDoc, collection } from 'firebase/firestore';
+import { addDoc, collection, doc, getDoc, getDocs } from 'firebase/firestore';
 import {
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
 } from 'firebase/auth';
 import { getDownloadURL, listAll, ref, uploadBytes } from 'firebase/storage';
+
+// For account
 export async function testFirebase(collectionName, id) {
   const docRef = doc(db, collectionName, id);
   return await getDoc(docRef);
@@ -27,6 +29,23 @@ export const signIn = async (email, password) => {
   }
 };
 
+export const createAccountWithEmailAndPassword = async (email, password) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password,
+    );
+    return { ok: true, data: userCredential };
+  } catch (err) {
+    if (err.code === 'auth/invalid-email') {
+      return { ok: false, errorMessage: 'email or password are invalid' };
+    }
+    return { ok: false, errorMessage: err.message };
+  }
+};
+
+// For image
 export const getImageByName = async (name) => {
   try {
     const imageRef = ref(storage, `images/${name}`);
@@ -46,22 +65,6 @@ export const getAllImages = async () => {
   return promise;
 };
 
-export const createAccountWithEmailAndPassword = async (email, password) => {
-  try {
-    const userCredential = await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password,
-    );
-    return { ok: true, data: userCredential };
-  } catch (err) {
-    if (err.code === 'auth/invalid-email') {
-      return { ok: false, errorMessage: 'email or password are invalid' };
-    }
-    return { ok: false, errorMessage: err.message };
-  }
-};
-
 export const uploadImage = async (file) => {
   const fileName = `${file.name.split('.')[0]}${Math.random().toString(16).slice(2)}.${file.name.split('.')[1]}`;
   const storageRef = ref(storage, 'images/' + fileName);
@@ -69,6 +72,7 @@ export const uploadImage = async (file) => {
   return fileName;
 };
 
+// For post
 export const addPostIntoFirebase = async ({
   imageName = 'this is for test',
   description,
@@ -81,5 +85,21 @@ export const addPostIntoFirebase = async ({
     comments: [],
   };
   const targetCollection = collection(db, 'posts');
-  await addDoc(targetCollection, docPayload);
+  return await addDoc(targetCollection, docPayload);
+};
+
+export const getSinglePostById = async (id) => {
+  const postSnap = await getDoc(doc(db, 'posts', id));
+  if (!postSnap.exists()) {
+    console.log('no document at all');
+    return;
+  }
+  return postSnap.data();
+};
+
+export const getAllDocFromCollection = async (collectionName) => {
+  const querySnapshot = await getDocs(collection(db, collectionName));
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, doc.data());
+  });
 };
--- src/store/post/index.js
+++ src/store/post/index.js
@@ -1,4 +1,9 @@
-import { addPostIntoFirebase, uploadImage } from '@/apis/firebase.js';
+import {
+  addPostIntoFirebase,
+  uploadImage,
+  getAllDocFromCollection,
+  getSinglePostById,
+} from '@/apis/firebase.js';
 import Swal from 'sweetalert2';
 
 export const post = {
@@ -6,6 +11,7 @@ export const post = {
   state() {
     return {
       showPopup: false,
+      posts: [],
     };
   },
   mutations: {
@@ -14,10 +20,15 @@ export const post = {
     },
   },
   actions: {
-    async uploadPost({ commit }, { file, description }) {
+    async fetchAllPosts() {
+      await getAllDocFromCollection('posts');
+    },
+    async uploadPost({ commit, state }, { file, description }) {
       Swal.showLoading();
       const imageName = await uploadImage(file);
-      await addPostIntoFirebase({ imageName, description });
+      const docRef = await addPostIntoFirebase({ imageName, description });
+      const post = await getSinglePostById(docRef.id);
+      state.posts.unshift(post);
       commit('CHANGE_SHOW_POPUP', false);
       Swal.close();
     },

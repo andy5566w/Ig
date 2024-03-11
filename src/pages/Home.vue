2dbb875cca84ccfaba57aa65486513ceaea2a8cf
andy.tsai <andy5566w@gmail.com>
2024-03-11 16:31:02 +0800

    [vue-project] render post in Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,29 @@
-<script setup></script>
+<template>
+  <div>
+    <PostList>
+      <PostItem v-for="post in filterPosts" v-bind="post" :key="post.id" />
+    </PostList>
 
-<template>Home</template>
+    <post-details v-if="showPostDetails" />
+    <post-upload v-if="showPostUpload" />
+  </div>
+</template>
+<script setup>
+import PostList from '../components/PostList.vue';
+import PostItem from '../components/PostItem.vue';
+import PostUpload from '@/components/PostUpload.vue';
+import { useStore } from 'vuex';
+import { computed } from 'vue';
+import PostDetails from '@/components/PostDetails.vue';
 
+const store = useStore();
+const showPostUpload = computed(() => store.state.post.showPopup);
+const showPostDetails = computed(() => store.state.post.showPostDetails);
+const filterPosts = computed(() =>
+  store.state.post.posts.sort(
+    (a, b) => b.publishedAt.seconds - a.publishedAt.seconds,
+  ),
+);
+store.dispatch('post/fetchAllPosts');
+</script>
 <style scoped></style>
